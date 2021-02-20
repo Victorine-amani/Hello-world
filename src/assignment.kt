63a892c66992fc51b5aa3206c853d96d1845fa40
Victorine-Amani <victorinenyagwala@gmail.com>
2021-02-20 21:37:13 +0300

    first commit

---
+++ src/assignment.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val fullName = "victorine amani"
+    var age = 19
+    var phoneNumber = "0791846426"
+    var weight = 78
+    val citizen = "kenyan"
+    println(fullName)
+    println(age)
+    println(phoneNumber)
+    println(weight)
+    println(citizen)
+}
